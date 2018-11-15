09e5e9ec26f168ff43a5e50ba8a954a02000c812
ydf_micro <ydf_micro@163.com>
2018-11-15 21:12:06 +0800

    Baum-Welch

--- venv/src/BaumWelch/ForwardBackward.py
+++ venv/src/BaumWelch/ForwardBackward.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def initQuintet():
-    Ob_state = np.array([0, 1, 0, 0, 1, 0, 1, 1])
+    Ob_status = np.array([0, 1, 0, 0, 1, 0, 1, 1])
 
-    hidden_state = np.array([1, 2, 3])
+    hidden_status = np.array([1, 2, 3])
 
     init_matrix = np.array([0.2, 0.4, 0.4])
 
-    trans_state = np.array([[0.5, 0.2, 0.3],
+    trans_status = np.array([[0.5, 0.2, 0.3],
                             [0.3, 0.5, 0.2],
                             [0.2, 0.3, 0.5]])
 
@@ -15,37 +15,37 @@ def initQuintet():
                              [0.4, 0.6],
                              [0.7, 0.3]])
 
-    return Ob_state, hidden_state, init_matrix, trans_state, confu_matrix
+    return Ob_status, hidden_status, init_matrix, trans_status, confu_matrix
 
-def Forward(result_F, init_matrix, trans_state, confu_matrix):
+def Forward(result_F, init_matrix, trans_status, confu_matrix):
     '''Forward'''
-    for i in range(len(Ob_state)):
+    for i in range(len(Ob_status)):
         if i == 0:
-            result_F[:, i] = init_matrix * confu_matrix[:, Ob_state[i]].flatten()
+            result_F[:, i] = init_matrix * confu_matrix[:, Ob_status[i]].flatten()
         else:
-            result_F[:, i] = np.dot(result_F[:, i - 1].flatten(), trans_state) * confu_matrix[:, Ob_state[i]].flatten()
+            result_F[:, i] = np.dot(result_F[:, i - 1].flatten(), trans_status) * confu_matrix[:, Ob_status[i]].flatten()
 
     return result_F
 
-def Backwward(result_B, trans_state, confu_matrix):
+def Backwward(result_B, trans_status, confu_matrix):
     '''Backward'''
-    for i in range(len(Ob_state) - 1, -1, -1):
-        if i == len(Ob_state) - 1:
+    for i in range(len(Ob_status) - 1, -1, -1):
+        if i == len(Ob_status) - 1:
             result_B[:, i] = 1
         else:
-            x = result_B[:, i + 1].flatten() * trans_state * confu_matrix[:, Ob_state[i + 1]].flatten()
+            x = result_B[:, i + 1].flatten() * trans_status * confu_matrix[:, Ob_status[i + 1]].flatten()
             result_B[:, i] = np.sum(x, axis=1)
 
     return result_B
 
 if __name__ == '__main__':
-    Ob_state, hidden_state, init_matrix, trans_state, confu_matrix = initQuintet()
+    Ob_status, hidden_status, init_matrix, trans_status, confu_matrix = initQuintet()
 
-    result_F = np.zeros((len(hidden_state), len(Ob_state)))
-    result_B = np.zeros((len(hidden_state), len(Ob_state)))
+    result_F = np.zeros((len(hidden_status), len(Ob_status)))
+    result_B = np.zeros((len(hidden_status), len(Ob_status)))
 
-    result_F = Forward(result_F, init_matrix, trans_state, confu_matrix)
-    result_B = Backwward(result_B, trans_state, confu_matrix)
+    result_F = Forward(result_F, init_matrix, trans_status, confu_matrix)
+    result_B = Backwward(result_B, trans_status, confu_matrix)
 
     print(result_F, '\n\n', result_B)
 
@@ -58,6 +58,6 @@ if __name__ == '__main__':
     xi_result = 0
     for i in range(0, 3):
         for j in range(0, 3):
-            xi_result += result_F[i, 4] * trans_state[i, j] * confu_matrix[j, Ob_state[5]] * result_B[j, 5]
-    xi = result_F[1, 4] * trans_state[1, 0] * confu_matrix[0, 0] * result_B[0, 5] / xi_result
+            xi_result += result_F[i, 4] * trans_status[i, j] * confu_matrix[j, Ob_status[5]] * result_B[j, 5]
+    xi = result_F[1, 4] * trans_status[1, 0] * confu_matrix[0, 0] * result_B[0, 5] / xi_result
     print('定义5时刻位于隐藏状态S2及6时刻位于隐藏状态S1的概率变量:', xi)
